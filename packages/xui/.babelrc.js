0b3ae2491c8d2c1ad2e3ec895f5e1c7ff243f4b5
黄波 <61913@any3.com>
2019-07-03 09:16:12 +0800

    fix: 更新

--- packages/xui/.babelrc.js
+++ packages/xui/.babelrc.js
@@ -50,7 +50,7 @@ module.exports = {
                     '@babel/preset-env',
                     {
                         useBuiltIns: 'usage',
-                        // corejs: 3,
+                        corejs: 3,
                     },
                 ],
                 'react-app',
